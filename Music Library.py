282f7df5ac31019b926774826d516381df5a4b49
robert11156 <robert11156@gmail.com>
2017-03-28 15:06:26 -0400

    Add files via upload

---
+++ Music Library.py
@@ -0,0 +1,67 @@
+
+class Music_Library():
+    # this sets up the variables in the object
+    def __init_(mp3):
+        mp3.artist=''
+        mp3.genre=''
+        mp3.albums=''
+        mp3.sex=''
+        mp3.song_length=''
+        mp3.multiple_artist=''
+
+# create an instance of this class
+main_music = Music_Library()
+main_music.artist = 'Drake'
+main_music.genre = 'Hip Hop/Rap'
+main_music.albums = 'More Life'
+main_music.sex = 'Male'
+main_music.song_length = '3:39'
+main_music.multiple_artist = 'Single'
+
+print("The artist of the music is",main_music.artist)
+print("The album name is,",main_music.albums,"Drake has 22 songs in the album","and my favorite song is Free Smoke.")
+
+
+class Music_Library():
+    # this sets up the variables in the object
+    def __init_(mp3):
+        mp3.artist=''
+        mp3.genre=''
+        mp3.albums=''
+        mp3.sex=''
+        mp3.song_length=''
+        mp3.multiple_artist=''
+
+# create an instance of this class
+secondary_music = Music_Library()
+secondary_music.artist = 'J Cole'
+secondary_music.genre = 'Hip Hop/Rap'
+secondary_music.albums = '2014 Forest Hills Drive'
+secondary_music.sex = 'Male'
+secondary_music.song_length = '4:53'
+secondary_music.multiple_artist = 'Single'
+
+print("The artist of the music is",secondary_music.artist)
+print("The album name is,",secondary_music.albums,"J Cole has 13 songs in the album","and my favorite song in the album is No Role Modelz.")
+
+class Music_Library():
+    # this sets up the variables in the object
+    def __init_(mp3):
+        mp3.artist=''
+        mp3.genre=''
+        mp3.albums=''
+        mp3.sex=''
+        mp3.song_length=''
+        mp3.multiple_artist=''
+
+# create an instance of this class
+Least_favorite_music = Music_Library()
+Least_favorite_music.artist = 'Justin Bieber'
+Least_favorite_music.genre = 'Pop'
+Least_favorite_music.albums = 'Purpose'
+Least_favorite_music.sex = 'Male'
+Least_favorite_music.song_length = '3:21'
+Least_favorite_music.multiple_artist = 'Single'
+
+print("The artist of the music is",Least_favorite_music.artist)
+print("The album name is,",Least_favorite_music.albums,"Justin Bieber has 13 songs in the album and is my least favorite artist.")
